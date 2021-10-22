f53af3b6012d7c3c90873f9426beef311b8434e5
Matias Gonzalo Donaire <matias.donaire.86@gmail.com>
2021-10-22 19:42:00 -0300

    Ultimos detalles y se agrego input nombre a contacto con sus estilos

--- proyecto_final_app/src/components/layout/contacto/Contacto.js
+++ proyecto_final_app/src/components/layout/contacto/Contacto.js
@@ -1,112 +1,153 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import {ContenedorFormulario, Formulario, Input, ContenedorBoton, Boton, ContenedorTexto} from './ElementosDeFormulario';
-
+import {
+  ContenedorFormulario,
+  Formulario,
+  Input,
+  ContenedorBoton,
+  Boton,
+  ContenedorTexto,
+  Titulo,
+  Parrafo
+} from "./ElementosDeFormulario";
 
 const expresionRegular = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
 
-const validators= (input) => {
-    let errors = {}
-    if(!input.email){
-        errors.email = "Se requiere un email"
-    } else if(!expresionRegular.test(input.email)){
-        errors.email = "Correo ingresado no valido"
-    }
-    if(!input.asunto){
-        errors.asunto = "Debe completar el asunto"
-    } else if(input.asunto.length < 10){
-        errors.asunto = "No puede tener menos de 10 caracteres"
-    }
-    if(!input.cuerpo){
-        errors.cuerpo = "Rellena con un mensaje"
-    } else if(input.cuerpo.length > 256){
-        errors.cuerpo = "No puede superar los 256 caracteres"
-    }
-    return errors;
-}
-
-
-
+const validators = (input) => {
+  let errors = {};
+  if (!input.username) {
+    errors.username = "Se requiere un nombre";
+  } else if (input.username.length < 3) {
+    errors.username = "No puede tener menos de 3 caracteres";
+  }
+  if (!input.email) {
+    errors.email = "Se requiere un email";
+  } else if (!expresionRegular.test(input.email)) {
+    errors.email = "Correo ingresado no valido";
+  }
+  if (!input.asunto) {
+    errors.asunto = "Debe completar el asunto";
+  } else if (input.asunto.length < 10) {
+    errors.asunto = "No puede tener menos de 10 caracteres";
+  }
+  if (!input.cuerpo) {
+    errors.cuerpo = "Rellena con un mensaje";
+  } else if (input.cuerpo.length > 256) {
+    errors.cuerpo = "No puede superar los 256 caracteres";
+  }
+  return errors;
+};
 
 const Contacto = () => {
+  const [state, setState] = useState({
+    username: "",  
+    email: "",
+    asunto: "",
+    cuerpo: "",
+  });
 
-    const [state, setState] = useState({
-        email: '',
-        asunto: '',
-        cuerpo: ''
-    })
-   
-    const [fails, setFails] = useState({
-        email: "Debes ingresar un correo valido",
-        asunto: "Debes ingresar un asunto",
-        cuerpo: "Debe ingresar un mensaje"
-    })
-    
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-    } 
-    const handleChange = (e)=>{
-        setState({
-            ...state,
-            [e.target.name]: e.target.value
-        })
-        
-        setFails(
-            validators({
-                ...state, 
-                [e.target.name]: e.target.value
-            })
-        )
+  const [fails, setFails] = useState({
+    username: "Debes ingresar un nombre",
+    email: "Debes ingresar un correo valido",
+    asunto: "Debes ingresar un asunto",
+    cuerpo: "Debe ingresar un mensaje",
+  });
 
-    }
-    return (
-        <>
-        
-        <ContenedorFormulario>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+
+    setFails(
+      validators({
+        ...state,
+        [e.target.name]: e.target.value,
+      })
+    );
+  };
+  return (
+    <>
+      <ContenedorFormulario>
         <ContenedorTexto>
-            <h1>Formulario de Contacto</h1>
-          <p>Escríbenos y en breve los pondremos en contacto contigo</p>
+          <Titulo>
+            Formulario de Contacto
+          </Titulo>
+          <p>Escríbenos y en breve nos pondremos en contacto contigo</p>
         </ContenedorTexto>
         <Formulario onSubmit={handleSubmit}>
-              
-              <Input 
-                 type="email"
-                 name="email"
-                 placeholder="Correo electronico"
-                 value={state.email}
-                 onChange={handleChange}
-                
-              />
-              {fails.email ? <p style={{color: 'red'}}> {fails.email}</p> : <p>Todo ok </p>}
-              <Input 
-                 type="text"
-                 name="asunto"
-                 placeholder="Asunto"
-                 value={state.asunto}     
-                 onChange={handleChange}          
-              />
-              {fails.asunto ? <p style={{color: 'red'}}> {fails.asunto}</p> : <p>Todo ok </p>}
-              <Input 
-                 type="textarea"
-                 name="cuerpo"
-                 placeholder="Mensaje"
-                 value={state.cuerpo}    
-                 onChange={handleChange}
-                 cols="30" rows="10"            
-              />
-               {fails.cuerpo ? <p style={{color: 'red'}}> {fails.cuerpo}</p> : <p>Todo ok </p>}
-            
-              <ContenedorBoton>
-                  <Boton  type="submit" to='/home' disabled={ fails.email || fails.asunto || fails.cuerpo ? true : false  }>Enviar</Boton>
-                  <Boton  type="submit" rojo  to='/home' >Cancelar</Boton>
-              </ContenedorBoton>
-              
-           </Formulario>
-        
-        </ContenedorFormulario>
-           
-        </>
-    );
-}
- 
+        <Input
+            type="text"
+            name="username"
+            placeholder="Tu nombre..."
+            value={state.username}
+            onChange={handleChange}
+          />
+          {fails.username ? (
+            <Parrafo rojo> {fails.username}</Parrafo>
+          ) : (
+            <Parrafo>👌ok </Parrafo>
+          )}
+          <Input
+            type="email"
+            name="email"
+            placeholder="Tu correo electrónico..."
+            value={state.email}
+            onChange={handleChange}
+          />
+          {fails.email ? (
+            <Parrafo rojo> {fails.email}</Parrafo>
+          ) : (
+            <Parrafo>👌ok </Parrafo>
+          )}
+          <Input
+            type="text"
+            name="asunto"
+            placeholder="El asunto del mensaje..."
+            value={state.asunto}
+            onChange={handleChange}
+          />
+          {fails.asunto ? (
+            <Parrafo rojo> {fails.asunto}</Parrafo>
+          ) : (
+            <Parrafo>👌ok </Parrafo>
+          )}
+          <Input
+            type="textarea"
+            name="cuerpo"
+            placeholder="Tu mensaje..."
+            value={state.cuerpo}
+            onChange={handleChange}
+            cols="30"
+            rows="10"
+          />
+          {fails.cuerpo ? (
+            <Parrafo rojo> {fails.cuerpo}</Parrafo>
+          ) : (
+            <Parrafo>👌ok </Parrafo>
+          )}
+
+          <ContenedorBoton>
+            <Boton
+              type="submit"
+              to="/home"
+              disabled={
+                fails.username || fails.email || fails.asunto || fails.cuerpo ? true : false
+              }
+            >
+              Enviar
+            </Boton>
+            <Boton type="submit" rojo to="/home">
+              Cancelar
+            </Boton>
+          </ContenedorBoton>
+        </Formulario>
+      </ContenedorFormulario>
+    </>
+  );
+};
+
 export default Contacto;
--- proyecto_final_app/src/components/layout/contacto/ElementosDeFormulario.js
+++ proyecto_final_app/src/components/layout/contacto/ElementosDeFormulario.js
@@ -15,16 +15,36 @@ const ContenedorFiltros = styled.div`
         }
     }
 `;
+
+const Titulo = styled.h1`
+margin: 0;
+text-align: center;
+padding-bottom: .5em;
+border-bottom: 2px solid #ec1d24;
+font-weight: 500;
+font-style: italic;
+font-size: 2em;
+text-transform: uppercase;
+letter-spacing: .1em;
+`;
+const Parrafo = styled.p`
+color: ${(props) => props.rojo ? '#ec1d24' :'#64C132'};
+font-size: 1.0em;
+@media(max-width: 60rem){ /* 950px */
+    font-size: 0.8em;
+}
+`;
+
 const ContenedorFormulario = styled.div`
 background: #fff;
 width: 60%;
 max-width: 50rem; /*1110px*/
-height: 90vh;
 max-height: 80rem;  /* 80px */
 overflow-y: auto;
-box-shadow: 0px 1.25rem 2.5rem rgba(0,0,0,.05);
+box-shadow: 0px 1.25rem 2.5rem rgba(0,0,0,.1);
 border-radius: 0.625rem; /* 10px */
-margin: 20px auto 20px auto;
+margin: 3em auto;
+padding: 2em;
 display: flex;
 flex-direction: column;
 justify-content: space-between;
@@ -39,17 +59,15 @@ z-index: 100;
 }
 `;
 const ContenedorTexto = styled.div`
-    font-size: 1.0rem; 
+    font-size: 1em; 
     border: none;
-    border-bottom: 2px solid #F6F1F0;
     outline: none;
  
     @media(max-width: 60rem){ /* 950px */
-        font-size: 0.8rem; /* 24px */
+        font-size: 0.5rem; /* 24px */
     }
 `;
 const Formulario = styled.form`
-    padding: 0 2.5rem; /* 40px */
  
     height: 80%;
     display: flex;
@@ -58,8 +76,7 @@ const Formulario = styled.form`
     input {
         width: 100%;
         text-align: center;
-        padding: 2.1rem 0;
-        font-family: 'Work Sans', sans-serif;
+        padding: 1em 0;
         &::placeholder {
             color: rgba(0,0,0,.2);
         }
@@ -71,14 +88,14 @@ const Formulario = styled.form`
 `;
  
 const Input = styled.input`
-    font-size: 1.0rem; /* 40px */
-    text-transform: uppercase;
+    font-size: 1.1em; /* 40px */
     border: none;
     border-bottom: 2px solid #F6F1F0;
     outline: none;
+    padding: 0;
  
     @media(max-width: 60rem){ /* 950px */
-        font-size: 0.8rem; /* 24px */
+        font-size: 0.7rem; /* 24px */
     }
 `;
  
@@ -89,22 +106,23 @@ const InputGrande = styled(Input)`
  
 const ContenedorBoton = styled.div`
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
     margin: 0;  /* 40px */
+    margin-top: 3em;
 `;
 const Boton = styled(Link)`
 
-    background: ${(props) => props.disabled ? 'gray' : props.rojo ? '#D30000' : '#006924'};
-    width: 15.62rem
-    margin-left: 1.25rem; /* 20px */
+    background: ${(props) => props.disabled ? 'gray' : props.rojo ? '#ec1d24' : '#64C132'};
+    width: 7em;
+    text-align: center;
     border: none;
-    border-radius: 0.625rem; /* 10px */
+    border-radius: .2em; /* 10px */
     color: #fff;
-    font-family: 'Work Sans', sans-serif;
     height: 2.0rem; /* 60px */
-    padding: 0.05rem 1.60rem; /* 20px 30px */
-    font-size: 1.0rem; /* 20px */
-    font-weight: 500;
+    padding: 1em; /* 20px 30px */
+    font-size: 1.3em; /* 20px */
+    font-weight: 300;
+    text-transform: uppercase;
     cursor: ${(props) => props.disabled ? 'no-drop' :'pointer'};
     pointer-events: ${(props) => props.disabled ? 'none' :'auto'};
     text-decoration: none;
@@ -112,7 +130,13 @@ const Boton = styled(Link)`
     justify-content: space-between;
     align-items: center;
     outline: none;
-    
+    display: flex;
+    justify-content: center;
+
+    @media(max-width: 60rem){ /* 950px */
+        padding: 0.7em;
+        font-size: 1.0rem; /* 24px */
+    }
 `;
 
-export {ContenedorFiltros,ContenedorFormulario, Formulario,ContenedorTexto, Input, InputGrande, ContenedorBoton, Boton};
+export {ContenedorFiltros,ContenedorFormulario, Formulario,ContenedorTexto, Input, InputGrande, ContenedorBoton, Boton, Titulo, Parrafo};
